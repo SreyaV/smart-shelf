55e2b57a81200b226b042b1ba2d8d269077f8267
Patrick Kao <patrick.d.kao@gmail.com>
2020-02-22 21:59:10 -0500

    First draft

---
+++ config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Server URL
+char URL[] = "http://65ac765d.ngrok.io/post";
+char HOST[] = "http://65ac765d.ngrok.io";
+
+// Wifi constants
+char NETWORK[] = "stratus";  // SSID for Wifi Network
+char PASSWORD[] = "jeremysux"; // Password for Wifi Networkbvg
+
+// buffers
+char requestBuffer[900];
+char responseBuffer[900];
+char body[600];
+
+// weather
+char id[] = "4930956";
+
+#endif
